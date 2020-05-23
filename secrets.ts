9966e4e74d69596a2f333dcc10e2669fb5ffbf4a
Lucas Oliveira <lucasolivei@icloud.com>
2020-05-23 17:20:44 -0300

    starting documentation

--- secrets.ts
+++ secrets.ts
@@ -1,7 +1,7 @@
 import { encode } from "./deps.ts";
 
-export function generateSecret(length?: number): string {
-  const secretLength = length || 32;
+export function generateSecret(size?: number): string {
+  const secretLength = size || 32;
   var bytes = crypto.getRandomValues(new Uint8Array(secretLength));
   return encode(bytes).replace(/=/g, "");
 }
--- totp.ts
+++ totp.ts
@@ -1,6 +1,11 @@
-import * as hotp from './hotp.ts'
+import * as hotp from './hotp.ts';
 import IOptions from "./mod.d.ts";
 
+/**
+ * 
+ * @param secret 
+ * @param options 
+ */
 export function generate(secret: string, options?: IOptions): string {
   const step = options?.step || 30;
   const counter = Math.floor(Date.now() / step / 1000);
@@ -8,19 +13,32 @@ export function generate(secret: string, options?: IOptions): string {
   return hotp.generate(secret, counter, options);
 }
 
-export function delta(secret: string, token: string, options?: IOptions) {
+/**
+ * @
+ * @param secret - private key 
+ * @param token - OTP 
+ * @param options 
+ * @returns the
+ */
+export function delta(secret: string, token: string, options?: IOptions): number | null {
   const step = options?.step || 30;
   const window = options?.window || 0;
   const counter = Math.floor(Date.now() / step / 1000);
 
   const delta = hotp.delta(secret, token, counter - window, {
     ...options,
-    window: window + window
+    window: window + window,
   });
 
   return delta !== null ? delta - window : null;
 }
 
-export function verify(secret: string, token: string, options?: IOptions) {
+/**
+ * 
+ * @param secret 
+ * @param token 
+ * @param options 
+ */
+export function verify(secret: string, token: string, options?: IOptions): boolean {
   return delta(secret, token, options) !== null;
 }
